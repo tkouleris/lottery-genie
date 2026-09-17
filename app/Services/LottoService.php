5049bf8b2562cbf6360cca1ae2edf0e1fa2f3b83
Thodoris Kouleris <kurokokoro@windowslive.com>
2026-09-17 04:22:04 +0300

    Add getLatestDrawDate method to LottoService for retrieving the latest draw details from stats files; refine header skipping logic from 3 to 4 rows

--- app/Services/LottoService.php
+++ app/Services/LottoService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Helpers\File;
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Support\Facades\Log;
@@ -29,7 +30,7 @@ class LottoService
 
                 foreach ($rows as $index => $row) {
                     // Skip header rows (first 3 rows) and non-numeric rows
-                    if ($index < 3 ) {
+                    if ($index < 4 ) {
                         continue;
                     }
 
@@ -168,7 +169,7 @@ class LottoService
 
                 foreach ($rows as $index => $row) {
                     // Skip header rows (first 3 rows) and non-numeric rows
-                    if ($index < 3 ) {
+                    if ($index < 4 ) {
                         continue;
                     }
 
@@ -256,6 +257,50 @@ class LottoService
 
         return [
             'numbers' => $finalNumbers,
+        ];
+    }
+
+    public function getLatestDrawDate(string $folder = 'stats/lotto'): array
+    {
+        $files = File::load_xlsx_files($folder);
+        $lastDraw = [];
+
+        foreach ($files as $file) {
+            try {
+                $spreadsheet = IOFactory::load($file);
+                $worksheet = $spreadsheet->getActiveSheet();
+                $rows = $worksheet->toArray();
+
+                foreach ($rows as $index => $row) {
+
+                    // Skip header rows (first 3 rows) and non-numeric rows
+                    if ($index < 4 ) {
+                        continue;
+                    }
+
+                    if(count($lastDraw) == 0) {
+                        $lastDraw = $row;
+                    }
+                    dd($lastDraw);
+                    $previous_date = Carbon::createFromFormat('d/m/Y', $lastDraw[1]);
+                    $current_date = Carbon::createFromFormat('d/m/Y', $row[1]);
+                    if($previous_date->lt($current_date)) {
+                        $lastDraw = $row;
+                    }
+                }
+            } catch (Exception $e) {
+                Log::error("Error reading file {$file}: " . $e->getMessage());
+            }
+        }
+        if(count($lastDraw) ==0) {
+            return [];
+        }
+
+        return [
+            'id' => $lastDraw[0],
+            'date' => $lastDraw[1],
+            'numbers' => [$lastDraw[2], $lastDraw[3], $lastDraw[4], $lastDraw[5], $lastDraw[6], $lastDraw[7]],
+            'joker' => [],
         ];
     }
 }
