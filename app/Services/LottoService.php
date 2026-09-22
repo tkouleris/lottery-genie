d6f6ca4f7fb32874c6899cb5b7c8e10ee5cd4872
Thodoris Kouleris <kurokokoro@windowslive.com>
2026-09-22 05:28:29 +0300

    Refactor LottoService to reuse `load_files` method for determining latest draw; streamline XLSX file processing logic

--- app/Services/LottoService.php
+++ app/Services/LottoService.php
@@ -204,37 +204,8 @@ class LottoService
         if($out) {
             return $out;
         }
+        $lastDraw = $this->load_files($folder)['lastDraw'];
 
-        $files = File::load_xlsx_files($folder);
-        $lastDraw = [];
-
-        foreach ($files as $file) {
-            try {
-                $spreadsheet = IOFactory::load($file);
-                $worksheet = $spreadsheet->getActiveSheet();
-                $rows = $worksheet->toArray();
-
-                foreach ($rows as $index => $row) {
-
-                    // Skip header rows (first 3 rows) and non-numeric rows
-                    if ($index < 4 ) {
-                        continue;
-                    }
-
-                    if(count($lastDraw) == 0) {
-                        $lastDraw = $row;
-                    }
-
-                    $previous_date = Carbon::createFromFormat('d/m/Y', $lastDraw[1]);
-                    $current_date = Carbon::createFromFormat('d/m/Y', $row[1]);
-                    if($previous_date->lt($current_date)) {
-                        $lastDraw = $row;
-                    }
-                }
-            } catch (Exception $e) {
-                Log::error("Error reading file {$file}: " . $e->getMessage());
-            }
-        }
         if(count($lastDraw) ==0) {
             return [];
         }
@@ -254,6 +225,7 @@ class LottoService
 
         $finalStatistics = [];
         $stats = [];
+        $lastDraw = [];
         foreach ($files as $file) {
             try {
                 $spreadsheet = IOFactory::load($file);
@@ -283,12 +255,22 @@ class LottoService
                             'numbers' => $numbers
                         ];
                     }
+
+                    if(count($lastDraw) == 0) {
+                        $lastDraw = $row;
+                    }
+
+                    $previous_date = Carbon::createFromFormat('d/m/Y', $lastDraw[1]);
+                    $current_date = Carbon::createFromFormat('d/m/Y', $row[1]);
+                    if($previous_date->lt($current_date)) {
+                        $lastDraw = $row;
+                    }
                 }
             } catch (Exception $e) {
                 Log::error("Error reading file {$file}: " . $e->getMessage());
             }
         }
-        return ['draws' => $finalStatistics, 'stats' => $stats];
+        return ['draws' => $finalStatistics, 'stats' => $stats, 'lastDraw' => $lastDraw];
     }
 
     private function getNextDrawDate()
