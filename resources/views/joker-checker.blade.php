dea2b27853ac02c036641ed1e52d46345a32dde1
Thodoris Kouleris <kurokokoro@windowslive.com>
2026-09-24 03:13:14 +0300

    Update Joker Checker image to use 'tzoker.jpg' instead of 'joker.jpg'

--- resources/views/joker-checker.blade.php
+++ resources/views/joker-checker.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <header class="text-center mb-12">
-        <img src="{{ asset('img/joker.jpg') }}" alt="Joker Logo" class="mx-auto" style="max-height: 150px;">
+        <img src="{{ asset('img/tzoker.jpg') }}" alt="Joker Logo" class="mx-auto" style="max-height: 150px;">
         <h1 class="text-3xl font-bold mt-4">Joker Combination Checker</h1>
         <p class="text-slate-400 text-lg">Check if your numbers have ever won in the past</p>
     </header>
